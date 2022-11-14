33cfd165b598b1b53ee363fbfde6f383bbda8ed4
Vaibhav-digiqt <vaibhav.chavda@digiqt.com>
2022-11-14 16:26:05 +0530

    changes

--- src/components/List.js
+++ src/components/List.js
@@ -1,33 +1,32 @@
-import React from 'react';
-import {FaTrash} from "react-icons/fa";
+// This is todolist task
 
-const List = ({items, removeItem}) => {
-    return (
-        <div className='container'>
-            {items.map((item) => {
-                const { id, title } = item;
-                return (
-                    <ul className='list-group list-group-flush' key={id}>
-                        <li className='list-group-item d-flex justify-content-between align-items-center'>
-                            {title}
-                            <div style={{ float: "right" }}>
-                                <button
-                                type='button'
-                                className='delete-btn'
-                                onClick={() => removeItem(id)}
-                                >
-                                    <FaTrash />
-                                </button>
-                            </div>
-                        </li>
-                    </ul>
-                )
-            })}
-        </div>
-    )
-}
+import React from "react";
+import { FaTrash } from "react-icons/fa";
+
+const List = ({ items, removeItem }) => {
+  return (
+    <div className="container">
+      {items.map((item) => {
+        const { id, title } = item;
+        return (
+          <ul className="list-group list-group-flush" key={id}>
+            <li className="list-group-item d-flex justify-content-between align-items-center">
+              {title}
+              <div style={{ float: "right" }}>
+                <button
+                  type="button"
+                  className="delete-btn"
+                  onClick={() => removeItem(id)}
+                >
+                  <FaTrash />
+                </button>
+              </div>
+            </li>
+          </ul>
+        );
+      })}
+    </div>
+  );
+};
 
 export default List;
-
-
-
